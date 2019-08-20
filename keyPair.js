e6afcae308c0ae952d30eaa54efd229dbfa47e2b
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-20 16:24:22 +0530

    user input methods removed

--- keyPair.js
+++ keyPair.js
@@ -1,7 +1,7 @@
 const HDKey = require('hdkey')
-const readline = require('readline-sync')
 const seed = require('./seed')
 const mnemonic = require('./mnemonic')
+const userInput = require('./userInput')
 
 const createParentKeyPair = (seedKey) => {
 
@@ -12,15 +12,9 @@ const createParentKeyPair = (seedKey) => {
 const generateChildKeyPairs = (parentKeyPair) => {
 
     let childKeyPairArray = [];
-    let derivePath;
-    let algoOption = getAlgoOption();
+    let algoOption = userInput.getAlgoOption();
+    let derivePath = userInput.getDerivePath(algoOption);
 
-    if (algoOption == 1) {
-        derivePath = readline.question("Enter derive path value \n sample format : (m/44/0/0/0/) \n");
-    }
-    else {
-        derivePath = getDerivePath();
-    }
 
     for (let addressIndex = 0; addressIndex < 10; addressIndex += 1) {
 
@@ -46,52 +40,7 @@ const printChildKeyPairArray = (childKeyPairArray) => {
     }
 }
 
-const getName = () => {
 
-    const option = readline.question("Choose (Default : Bitcoin) \n 1. Bitcoin \n 2. Ethereum \n");
-    if (option == 2)
-        return 'Ether';
-    return 'Bitcoin';
-}
-
-const getAlgoOption = () => {
-
-    const algoOption = readline.question("Choose (Default : BIP32) \n 1. BIP32 \n 2. BIP44 \n");
-    if (algoOption == 2)
-        return 2;
-    return 1;
-}
-const getCoinType = () => {
-
-    let cointType;
-    cointType = readline.question("Choose (Default : BitCoin) \n 1. BitCoinn \n 2. Ethereum \n");
-    if (cointType == 1)
-        return 0;
-    else if (cointType == 2)
-        return 60;
-    return 0;
-}
-
-const getAccountNo = () => {
-    let accountNo = readline.question("Enter Account Number Index:");
-    return accountNo;
-}
-
-const getChainName = () => {
-    let chainNameOption = readline.question("Choose (Default : External) \n 1. External(0) \n 2. Internal(1) \n");
-
-    if (chainNameOption == 2)
-        return 1;
-    return 0;
-}
-
-const getDerivePath = () => {
-    let coinType = getCoinType();
-    let accountNo = getAccountNo();
-    let chainName = getChainName();
-    let derivePath = 'm/44/' + coinType + '/' + accountNo + '/' + chainName + '/';
-    return derivePath;
-}
 
 module.exports = {
     createParentKeyPair,
