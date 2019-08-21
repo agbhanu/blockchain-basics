5e2ef0e21535a6a40b2bf24a7b195a2ee7de33ef
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-21 11:44:44 +0530

    enum created and conditions added

--- address/addressAdapter.js
+++ address/addressAdapter.js
@@ -1,7 +1,29 @@
-function AddressAdapter(addressObject,publicKey){
-    return addressObject.getAddressFromPublicKey(publicKey);
+const { BitcoinAddress } = require('./bitcoinAddress')
+const { EthereumAddress } = require('./ethereumAddress')
+
+// enum to reperesent different coin types
+const CoinName = Object.freeze({
+    BITCOIN: 'Bitcoin',
+    ETHER: 'Ether'
+})
+
+function AddressAdapter(coinName, childKeyPairArray) {
+
+    let childKeyPairArrayWithAddress = childKeyPairArray.map((keyPair) => {
+        let address;
+        let publicKey = keyPair.childKeyPair.publicKey;
+        if (coinName === CoinName.BITCOIN) {
+            address = BitcoinAddress().getAddressFromPublicKey(publicKey);
+        }
+        else if (coinName === CoinName.ETHER) {
+            address = EthereumAddress().getAddressFromPublicKey(publicKey);
+        }
+        keyPair['address'] = address;
+        return keyPair;
+    })
+    return childKeyPairArrayWithAddress;
 }
 
 module.exports = {
-    AddressAdapter : AddressAdapter
+    AddressAdapter: AddressAdapter
 }
