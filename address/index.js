03e985c0e700e99874932adb4381844673054069
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-21 11:44:21 +0530

    condition moved to addressAdapter.js file

--- address/index.js
+++ address/index.js
@@ -1,21 +1,7 @@
 const { AddressAdapter } = require('./addressAdapter')
-const { BitcoinAddress } = require('./bitcoinAddress')
-const { EthereumAddress } = require('./ethereumAddress')
 
 const addAddressInChildKeyPairArray = (coinName, childKeyPairArray) => {
-
-    childKeyPairArray.forEach((keyPair) => {
-        let address;
-        let publicKey = keyPair.childKeyPair.publicKey;
-        if (coinName === 'Bitcoin') {
-            address = AddressAdapter(BitcoinAddress(), publicKey);
-        }
-        else if (coinName === 'Ether') {
-            address = AddressAdapter(EthereumAddress(), publicKey);
-        }
-        keyPair['address'] = address;
-    })
-    return childKeyPairArray;
+    return AddressAdapter(coinName,childKeyPairArray);
 }
 
 module.exports = {
