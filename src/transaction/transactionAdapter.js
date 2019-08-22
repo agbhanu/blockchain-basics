c278ecd6d99302625059244c10737bb8f7631833
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-22 17:14:45 +0530

    constant data is used

--- src/transaction/transactionAdapter.js
+++ src/transaction/transactionAdapter.js
@@ -1,20 +1,13 @@
 import {BitcoinTransaction} from './bitcoinTransaction'
 import {EthereumTransaction} from './etherTransaction'
-
-// enum to reperesent different coin types
-const CoinName = Object.freeze({
-    BITCOIN: 'Bitcoin',
-    ETHER: 'Ether'
-})
+import * as constants from '../constants'
 
 export async function TransactionAdapter(coinName, senderPrivateKey, senderAddress, recieverAddress) {
 
-    let txHash;
-    if (coinName === CoinName.BITCOIN) {
-        txHash = await BitcoinTransaction().createTransaction(senderPrivateKey,senderAddress,recieverAddress)
+    if(!constants.CoinAddressMap.hasOwnProperty(coinName)){
+        throw new Error('Not a valid coin name');
     }
-    else if (coinName === CoinName.ETHER) {
-        txHash = await EthereumTransaction().createTransaction(senderPrivateKey,senderAddress,recieverAddress)
-    }
+
+    const txHash = await constants.CoinTransactionMap[coinName](senderPrivateKey,senderAddress,recieverAddress);
     return txHash;
 }
