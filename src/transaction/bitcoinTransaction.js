5172fefa75138d694673e90e62b5b47aaccb2299
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-26 12:10:31 +0530

    txAmount and balance are wrapped inside big number

--- src/transaction/bitcoinTransaction.js
+++ src/transaction/bitcoinTransaction.js
@@ -10,83 +10,79 @@ import WAValidator from 'wallet-address-validator'
 import BN from 'bignumber.js'
 import * as constants from '../constants'
 
-export function BitcoinTransaction() {
+export const createBitcoinTransaction = (senderPrivateKey, senderAddress, receiverAddress) => {
+    return new Promise(async (resolve, reject) => {
 
-    return {
-        createTransaction: (senderPrivateKey, senderAddress, receiverAddress) => {
-            return new Promise(async (resolve, reject) => {
+        let txHash;
+        const privateKeyWIF = bitcore.PrivateKey.fromWIF(senderPrivateKey.toString('hex'));
 
-                let txHash;
-                const privateKeyWIF = bitcore.PrivateKey.fromWIF(senderPrivateKey.toString('hex'));
+        // convert bitcoins to sathosis
+        const txAmount = (new BN(userInput.getBitcoins())).multipliedBy(1e8).toNumber();
 
-                // convert bitcoins to sathosis
-                const txAmount = (new BN(userInput.getBitcoins())).multipliedBy(1e8).toNumber();
+        // check amount entered correctly or not
+        if (isNaN(txAmount)) {
+            return reject('Not valid amount')
+        }
 
-                // check amount entered correctly or not
-                if(isNaN(txAmount)){
-                    return reject('Not valid amount')
-                }
+        // check tx amount should be greater than dust value
+        if (txAmount < constants.DUST_VALUE) {
+            return reject('Amount want to send is too less ( please send more than 600 satoshis)');
+        }
 
-                // check tx amount should be greater than dust value
-                if(txAmount<constants.DUST_VALUE){
-                    return reject('Amount want to send is too less ( please send more than 600 satoshis)');
-                }
+        let minerFee;
+        try {
+            const feePerByte = await getMiningFeePerByte();
+            const txSizeInBytes = await getEstimatedTxSize(senderAddress, receiverAddress, senderPrivateKey, txAmount);
+            minerFee = txSizeInBytes * feePerByte;
+        } catch (error) {
+            return reject(error);
+        }
+        insight.getUnspentUtxos(senderAddress, (error, utxos) => {
 
-                let minerFee;
+            if (error) {
+                return reject(error);
+            }
+
+            if (utxos.length === 0) {
+                //if no transactions have happened, there is no balance on the address.
+                return reject("You don't have enough Satoshis to cover the miner fee.");
+            }
+
+            let balance = 0;
+            for (var i = 0; i < utxos.length; i++) {
+                balance += new BN(utxos[i]['satoshis']).toNumber();
+            }
+
+            if ((balance - txAmount - minerFee) > 0) {
+
                 try {
-                    const feePerByte = await getMiningFeePerByte();
-                    const txSizeInBytes = await getEstimatedTxSize(senderAddress, receiverAddress, senderPrivateKey, txAmount);
-                    minerFee = txSizeInBytes * feePerByte;
-                } catch (error) {
-                    return reject(error);
-                }
-                insight.getUnspentUtxos(senderAddress, (error, utxos) => {
+                    let tx = bitcore.Transaction()
+                    tx.from(utxos)
+                    tx.to(receiverAddress, txAmount)
+                    tx.change(senderAddress);
+                    tx.fee(minerFee);
+                    tx.sign(privateKeyWIF);
 
-                    if (error) {
-                        return reject(error);
-                    }
-
-                    if (utxos.length === 0) {
-                        //if no transactions have happened, there is no balance on the address.
-                        return reject("You don't have enough Satoshis to cover the miner fee.");
-                    }
-
-                    let balance = 0;
-                    for (var i = 0; i < utxos.length; i++) {
-                        balance += utxos[i]['satoshis'];
-                    }
-
-                    if ((balance - txAmount - minerFee) > 0) {
-
-                        try {
-                            let tx = bitcore.Transaction()
-                            tx.from(utxos)
-                            tx.to(receiverAddress, txAmount)
-                            tx.change(senderAddress);
-                            tx.fee(minerFee);
-                            tx.sign(privateKeyWIF);
-
-                            insight.broadcast(tx.serialize(), (err, txId) => {
-                                if (err) {
-                                    return reject(err);
-                                }
-                                //console.log(`successful broadcast : ${txId}`);
-                                txHash = txId;
-                                return resolve(txHash);
-                            });
-
-                        } catch (err) {
+                    insight.broadcast(tx.serialize(), (err, txId) => {
+                        if (err) {
                             return reject(err);
                         }
+                        //console.log(`successful broadcast : ${txId}`);
+                        txHash = txId;
+                        return resolve(txHash);
+                    });
 
-                    } else {
-                        return reject("You don't have enough Satoshis to cover the miner fee.");
-                    }
-                });
-            })
-        }
-    }
+                } catch (err) {
+                    return reject(err);
+                }
+
+            } else {
+                return reject("You don't have enough Satoshis to cover the miner fee.");
+            }
+        });
+    })
 }
+
 
 const getEstimatedTxSize = (senderAddress, receiverAddress, senderPrivateKey, txAmount) => {
 
--- src/transaction/etherTransaction.js
+++ src/transaction/etherTransaction.js
@@ -1,77 +1,74 @@
 import Web3 from 'web3'
-import {Transaction as Tx} from 'ethereumjs-tx'
+import { Transaction as Tx } from 'ethereumjs-tx'
 import * as userInput from '../userInput'
+import BN from 'bignumber.js'
 import dotenv from 'dotenv'
 dotenv.config();
 
-export function EthereumTransaction() {
-  return {
-    createTransaction: (senderPrivateKey, senderAddress, receiverAddress) => {
+export const createEthereumTransaction = (senderPrivateKey, senderAddress, receiverAddress) => {
 
-      return new Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
 
-        const rpcURL = process.env.TESTNET_PROVIDER_ETHEREUM;
-        const web3 = new Web3(rpcURL);
-        const txAmountInEthers = userInput.getEthers();
+    const rpcURL = process.env.TESTNET_PROVIDER_ETHEREUM;
+    const web3 = new Web3(rpcURL);
+    const txAmountInEthers = userInput.getEthers();
 
-        // check amount entered correctly or not
-        if(isNaN(txAmountInEthers)){
-          return reject('Not valid amount');
-        }
-
-        const txAmount = web3.utils.toWei(txAmountInEthers, 'ether');
-        if (web3.utils.toChecksumAddress(senderAddress) && web3.utils.toChecksumAddress(receiverAddress)) {
-          let txHash;
-
-          try {
-            const balance = await web3.eth.getBalance(senderAddress);
-            const gasPrice = await web3.eth.getGasPrice();
-            const nonce = await web3.eth.getTransactionCount(senderAddress);
-            const gasEstimate = await web3.eth.estimateGas({
-              to: receiverAddress,
-              from: senderAddress,
-              nonce: web3.utils.toHex(nonce)
-            });
-
-
-            if ((balance - txAmount) > 0) {
-              const rawTx = {
-                nonce: web3.utils.toHex(nonce+1),
-                gasPrice: web3.utils.toHex(gasPrice),
-                gasLimit: web3.utils.toHex(gasEstimate),
-                to: receiverAddress,
-                value: web3.utils.toHex(txAmount)
-              }
-
-              const tx = new Tx(rawTx, { chain: 'rinkeby', hardfork: 'petersburg' })
-              tx.sign(senderPrivateKey)
-
-              const serializedTx = tx.serialize()
-              const raw = '0x' + serializedTx.toString('hex')
-
-              // Broadcast the transaction
-              web3.eth.sendSignedTransaction(raw, (err, txId) => {
-                if (err) {
-                  return reject(err);
-                }
-                else {
-                  //console.log(`txHash : ${txId}`);
-                  txHash = txId;
-                  return resolve(txHash);
-                }
-              });
-            } else {
-              return reject("Not enough amount in wallet");
-            }
-          } catch (err) {
-            console.log(err);
-          }
-        } else {
-          return reject("Sender or reciever address is not valid");
-        }
-
-
-      })
+    // check amount entered correctly or not
+    if (isNaN(txAmountInEthers)) {
+      return reject('Not valid amount');
     }
-  }
+
+    const txAmount = new BN(web3.utils.toWei(txAmountInEthers, 'ether')).toNumber();
+    if (web3.utils.toChecksumAddress(senderAddress) && web3.utils.toChecksumAddress(receiverAddress)) {
+      let txHash;
+
+      try {
+        const balance = new BN(await web3.eth.getBalance(senderAddress)).toNumber();
+        const gasPrice = await web3.eth.getGasPrice();
+        const nonce = await web3.eth.getTransactionCount(senderAddress);
+        const gasEstimate = await web3.eth.estimateGas({
+          to: receiverAddress,
+          from: senderAddress,
+          nonce: web3.utils.toHex(nonce)
+        });
+
+
+        if ((balance - txAmount) > 0) {
+          const rawTx = {
+            nonce: web3.utils.toHex(nonce + 1),
+            gasPrice: web3.utils.toHex(gasPrice),
+            gasLimit: web3.utils.toHex(gasEstimate),
+            to: receiverAddress,
+            value: web3.utils.toHex(txAmount)
+          }
+
+          const tx = new Tx(rawTx, { chain: 'rinkeby', hardfork: 'petersburg' })
+          tx.sign(senderPrivateKey)
+
+          const serializedTx = tx.serialize()
+          const raw = '0x' + serializedTx.toString('hex')
+
+          // Broadcast the transaction
+          web3.eth.sendSignedTransaction(raw, (err, txId) => {
+            if (err) {
+              return reject(err);
+            }
+            else {
+              //console.log(`txHash : ${txId}`);
+              txHash = txId;
+              return resolve(txHash);
+            }
+          });
+        } else {
+          return reject("Not enough amount in wallet");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      return reject("Sender or reciever address is not valid");
+    }
+
+
+  })
 }
