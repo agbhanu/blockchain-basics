456ed1f612ce500e5ce209f94c0f71a2e1e80316
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-26 17:37:57 +0530

    createTransaction function is modularized and constants added

--- src/transaction/bitcoinTransaction.js
+++ src/transaction/bitcoinTransaction.js
@@ -1,120 +1,108 @@
 import * as bitcoin from 'bitcoinjs-lib'
 import bitcore from 'bitcore-lib'
+import { DUST_VALUE, SATOSHIS, BITCOIN_NETWORK } from '../constants'
 import Explorer from 'bitcore-explorers'
-const insight = new Explorer.Insight('testnet')
 import * as userInput from '../userInput'
 import dotenv from 'dotenv'
 dotenv.config()
 import axios from 'axios'
 import WAValidator from 'wallet-address-validator'
 import BN from 'bignumber.js'
-import * as constants from '../constants'
 
 export const createBitcoinTransaction = (senderPrivateKey, senderAddress, receiverAddress) => {
     return new Promise(async (resolve, reject) => {
 
-        let txHash;
-        const privateKeyWIF = bitcore.PrivateKey.fromWIF(senderPrivateKey.toString('hex'));
-
-        // convert bitcoins to sathosis
-        const txAmount = (new BN(userInput.getBitcoins())).multipliedBy(1e8).toNumber();
-
-        // check amount entered correctly or not
-        if (isNaN(txAmount)) {
-            return reject('Not valid amount')
-        }
-
-        // check tx amount should be greater than dust value
-        if (txAmount < constants.DUST_VALUE) {
-            return reject('Amount want to send is too less ( please send more than 600 satoshis)');
-        }
-
-        let minerFee;
         try {
+
+            const txAmount = fromBitcoinsToSatoshis(userInput.getBitcoins());
+            if (isNaN(txAmount)) {
+                throw new Error("Not valid amount");
+            }
+
+            if (txAmount < DUST_VALUE) {
+                throw new Error("Amount want to send is too less ( please send more than 600 satoshis)");
+            }
+
+            if (!(isValidAddress(senderAddress, 'BTC', 'testnet') && isValidAddress(receiverAddress, 'BTC', 'testnet'))) {
+                throw new Error("Sender or reciever address is not valid")
+            }
+
+            const insight = new Explorer.Insight(BITCOIN_NETWORK);
+            const privateKeyWIF = bitcore.PrivateKey.fromWIF(senderPrivateKey.toString('hex'));
             const feePerByte = await getMiningFeePerByte();
-            const txSizeInBytes = await getEstimatedTxSize(senderAddress, receiverAddress, senderPrivateKey, txAmount);
-            minerFee = txSizeInBytes * feePerByte;
+
+            const utxos = await getUtxos(insight, senderAddress);
+            const balance = getBalance(insight, utxos);
+            const tx = getTx(utxos, receiverAddress, txAmount, senderAddress);
+
+            const txSizeInBytes = tx._estimateSize();
+            const minerFee = txSizeInBytes * feePerByte;
+
+            if ((balance - txAmount - minerFee) < 0) {
+                throw new Error("You don't have enough Satoshis to cover the miner fee.")
+            }
+
+            // add mining fee and sign tx with private key
+            tx.fee(minerFee);
+            tx.sign(privateKeyWIF);
+
+            const txHash = await broadcastTx(insight, tx);
+            return resolve(txHash);
         } catch (error) {
             return reject(error);
         }
-        insight.getUnspentUtxos(senderAddress, (error, utxos) => {
-
-            if (error) {
-                return reject(error);
-            }
-
-            if (utxos.length === 0) {
-                //if no transactions have happened, there is no balance on the address.
-                return reject("You don't have enough Satoshis to cover the miner fee.");
-            }
-
-            let balance = 0;
-            for (var i = 0; i < utxos.length; i++) {
-                balance += new BN(utxos[i]['satoshis']).toNumber();
-            }
-
-            if ((balance - txAmount - minerFee) > 0) {
-
-                try {
-                    let tx = bitcore.Transaction()
-                    tx.from(utxos)
-                    tx.to(receiverAddress, txAmount)
-                    tx.change(senderAddress);
-                    tx.fee(minerFee);
-                    tx.sign(privateKeyWIF);
-
-                    insight.broadcast(tx.serialize(), (err, txId) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        //console.log(`successful broadcast : ${txId}`);
-                        txHash = txId;
-                        return resolve(txHash);
-                    });
-
-                } catch (err) {
-                    return reject(err);
-                }
-
-            } else {
-                return reject("You don't have enough Satoshis to cover the miner fee.");
-            }
-        });
     })
 }
 
+export const fromBitcoinsToSatoshis = (txAmount) => {
+    return new BN(txAmount).multipliedBy(1e8).toNumber();
+}
 
-const getEstimatedTxSize = (senderAddress, receiverAddress, senderPrivateKey, txAmount) => {
-
+export const getUtxos = (insight, senderAddress) => {
     return new Promise((resolve, reject) => {
-        const validSenderAddress = WAValidator.validate(senderAddress, 'BTC', 'testnet')
-        const validReceiverAddress = WAValidator.validate(receiverAddress, 'BTC', 'testnet');
-        if (validSenderAddress && validReceiverAddress) {
-            insight.getUnspentUtxos(senderAddress, (error, utxos) => {
+        insight.getUnspentUtxos(senderAddress, (err, utxos) => {
 
-                if (error)
-                    return reject(error);
+            if (err)
+                throw new Error(err);
 
-
-                try {
-                    let tx = bitcore.Transaction();
-                    tx.from(utxos)
-                    tx.to(receiverAddress, txAmount)
-                    tx.change(senderAddress)
-
-                    return resolve(tx._estimateSize());
-                } catch (error) {
-                    return reject(error);
-                }
-            })
-        } else {
-            return reject("Sender or reciever address is not valid");
-        }
+            return resolve(utxos);
+        })
     })
 }
 
-const getMiningFeePerByte = async () => {
+export const getBalance = (insight, utxos) => {
+    let balance = 0;
+    for (var i = 0; i < utxos.length; i++) {
+        balance += new BN(utxos[i][SATOSHIS]).toNumber();
+    }
+    return balance;
+}
+
+export const isValidAddress = (address, coinName, network) => {
+    return (WAValidator.validate(address, coinName, network));
+}
+
+export const getTx = (utxos, receiverAddress, amount, changeAddress) => {
+    const tx = bitcore.Transaction();
+    tx.from(utxos)
+    tx.to(receiverAddress, amount)
+    tx.change(changeAddress)
+    return tx;
+}
+
+export const getMiningFeePerByte = async () => {
     const feeObj = await axios.get(process.env.MINOR_FEE_CALCULATOR);
     //console.log(feeObj.data.halfHourFee);
     return feeObj.data.halfHourFee;
+}
+
+export const broadcastTx = (insight, tx) => {
+    return new Promise((resolve, reject) => {
+        insight.broadcast(tx.serialize(), (err, txHash) => {
+            if (err) {
+                throw new Error(err);
+            }
+            return resolve(txHash);
+        });
+    })
 }
