0360b2bd50106d09151c63d632af4e348e8dabdb
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-26 12:09:02 +0530

    hard coded values removed

--- src/constants.js
+++ src/constants.js
@@ -1,24 +1,26 @@
-import {BitcoinAddress} from './address/bitcoinAddress'
-import {EthereumAddress} from './address/ethereumAddress'
-import { BitcoinTransaction } from './transaction/bitcoinTransaction';
-import { EthereumTransaction } from './transaction/etherTransaction';
+import {getBitcoinAddressFromPublicKey} from './address/bitcoinAddress'
+import {getEthereumAddressFromPublicKey} from './address/ethereumAddress'
+import { createBitcoinTransaction } from './transaction/bitcoinTransaction';
+import { createEthereumTransaction } from './transaction/etherTransaction';
+
+export const DUST_VALUE = 600
+export const BITCOIN = 'Bitcoin'
+export const ETHER = 'Ether'
 
 export const CoinAddressMap = Object.freeze({
-    'Bitcoin' : BitcoinAddress().getAddressFromPublicKey,
-    'Ether' : EthereumAddress().getAddressFromPublicKey,
+    [BITCOIN] : getBitcoinAddressFromPublicKey,
+    [ETHER] : getEthereumAddressFromPublicKey,
     
 })
 
 export const CoinTransactionMap = Object.freeze({
-    'Bitcoin' : BitcoinTransaction().createTransaction,
-    'Ether' : EthereumTransaction().createTransaction,
+    [BITCOIN] : createBitcoinTransaction,
+    [ETHER] : createEthereumTransaction,
     
 })
 
 export const CoinName = Object.freeze({
-    1: 'Bitcoin',
-    2: 'Ether',
+    1: BITCOIN,
+    2: ETHER,
     default: null
 })
-
-export const DUST_VALUE = 600
