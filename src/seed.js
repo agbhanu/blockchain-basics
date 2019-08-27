5a257e0228ad53fb2ff047d61e6822d099205af1
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-27 10:52:30 +0530

    signature of generateSeed method changed

--- src/seed.js
+++ src/seed.js
@@ -1,17 +1,6 @@
 import Mnemonic from 'bitcore-mnemonic'
-import * as userInput from './userInput'
 
-export const generateSeed = (mnemonicCode) => {
-
-  const userOption = userInput.getOptionForPassphrase();
-  let seed;
-  if (userOption === 'yes') {
-    const passphrase = userInput.getPassphrase();
-    seed = new Mnemonic(mnemonicCode).toHDPrivateKey(passphrase);
-  }
-  else {
-    seed = new Mnemonic(mnemonicCode).toHDPrivateKey();
-  }
-
+export const generateSeed = (mnemonicCode,passPhrase) => {
+  const seed = new Mnemonic(mnemonicCode).toHDPrivateKey(passPhrase);
   return seed;
 }
